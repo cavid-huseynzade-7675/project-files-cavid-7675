20daacdc21f630c855ba73be3a57a500633196e0
cavid-huseynzade-7675 <alicavid826@gmai;.com>
2020-05-07 18:39:15 +0400

    7-ə qalıqsız bölünən ədədləri gosreren proqram

---
+++ projects/Java-project-Cavid-7675/src/com/main/NewClass.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.main;
+
+/**
+ *
+ * @author Cavid
+ */
+public class NewClass {
+    public static void main(String[] args) {
+         for (int i = 1; i <= 100; i++) {
+        int a=i%7;
+      
+         if(a==0) {  
+             System.out.println(i+  "  7 e qaliqsiz bolunur");
+        }
+    }
+}
+}
