c574d3fa849a5798fea5b3e7e08839082c1fd0a6
cavid-huseynzade-7675 <alicavid826@gmai;.com>
2020-05-07 17:18:16 +0400

    dairenin sahesini tapan proqram

---
+++ projects/Java-project-Cavid-7675/src/com/main/DaireninSahesi.java
@@ -0,0 +1,19 @@
+
+package com.main;
+
+import java.util.Scanner;
+
+
+public class DaireninSahesi {
+
+    public static void main(String[] args) {
+        double pi = 3.14;
+        Scanner s = new Scanner(System.in);
+        System.out.println("Dairenin radiusunu de");
+        int radius = s.nextInt();
+        int radiusunkvadrati = radius * radius;
+ double  daireninsahesi = pi * radiusunkvadrati;
+        System.out.println("dairenin sahesi " + daireninsahesi + " dir");
+    }
+
+}
