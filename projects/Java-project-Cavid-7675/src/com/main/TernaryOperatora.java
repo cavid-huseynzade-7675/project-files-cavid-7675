668c7fd93858208c54d8256f905b33e315f84c01
cavid-huseynzade-7675 <alicavid826@gmai;.com>
2020-05-07 16:21:20 +0400

    ternary operator
    yazdigim birinci kod ternary operatorda

---
+++ projects/Java-project-Cavid-7675/src/com/main/TernaryOperatora.java
@@ -0,0 +1,13 @@
+package com.main;
+
+public class TernaryOperatora {
+
+    public static void main(String[] args) {
+
+        int x = 4;
+        boolean b = (x == 4) ? true : false;
+
+        System.out.println(b);
+    }
+
+}
