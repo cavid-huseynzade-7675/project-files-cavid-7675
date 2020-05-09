a9359a61c5457b36aa6a75bf1a1fe50df142d687
cavid-huseynzade-7675 <alicavid826@gmai;.com>
2020-05-09 16:26:53 +0400

    type casting
    double dan int e kecis

---
+++ projects/Java-project-Cavid-7675/src/com/main/TypeCasting.java
@@ -0,0 +1,14 @@
+package com.main;
+
+public class TypeCasting {
+
+    public static void main(String[] args) {
+        double x = 58.94;
+        
+        System.out.println((int)x);
+                
+        
+    }
+   
+  
+}
---
+++ projects/Java-project-Cavid-7675/src/com/main/TypeCasting2.java
@@ -0,0 +1,11 @@
+package com.main;
+
+public class TypeCasting2 {
+
+    public static void main(String[] args) {
+       int a = 85956;
+
+        System.out.println((byte) a);
+    }
+
+}
