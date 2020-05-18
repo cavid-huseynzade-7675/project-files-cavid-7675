ff28e60cd267aa46a42ae423bda4869af8f1ec05
cavid-huseynzade-7675 <alicavid826@gmai;.com>
2020-05-18 15:34:15 +0400

    Update DaireninSahesi.java

--- projects/Java-project-Cavid-7675/src/com/main/DaireninSahesi.java
+++ projects/Java-project-Cavid-7675/src/com/main/DaireninSahesi.java
@@ -8,8 +8,8 @@ public class DaireninSahesi {
         double pi = 3.14;
         Scanner s = new Scanner(System.in);
         System.out.println("Dairenin radiusunu de");
-        int radius = s.nextInt();
-        int radiusunkvadrati = radius * radius;
+        double radius = s.nextDouble();
+        double radiusunkvadrati = radius * radius;
         double daireninsahesi = pi * radiusunkvadrati;
         System.out.println("dairenin sahesi " + daireninsahesi + " dir");
     }
