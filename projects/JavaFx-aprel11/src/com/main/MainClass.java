5e7c171405ec8d2954a5ce7c65a68d16eac4e2e0
cavid-huseynzade-7675 <alicavid826@gmai;.com>
2020-06-24 10:14:36 +0400

    ilk pencerem

---
+++ projects/JavaFx-aprel11/src/com/main/MainClass.java
@@ -0,0 +1,25 @@
+package com.main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainClass extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage s) throws Exception {
+        s.setTitle("ilk pencerem");
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("main.fxml"));
+        Parent root=loader.load();
+        Scene scene =new Scene(root);
+        s.setScene(scene);
+        s.show();
+    }
+
+}
