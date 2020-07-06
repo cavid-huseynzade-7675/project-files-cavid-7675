fd50c6eed2d2c9d1d695367ccc8b630e3d785dd0
cavid-huseynzade-7675 <alicavid826@gmai;.com>
2020-07-06 19:11:58 +0400

    ImageView

--- projects/ImageView/src/imageview/Flag.java
+++ projects/ImageView/src/imageview/Flag.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package imageview;
 
-/**
- *
- * @author Cavid
- */
-public class Flag {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Flag extends Application{
+ 
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+
+        
+    @Override
+    public void start(Stage s) throws Exception {
+     s.setTitle("Flag");
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("flag.fxml"));
+        Parent root=loader.load();
+        Scene scene= new Scene(root);
+        s.setScene(scene);
+        s.show();
+    }   
     
 }
---
+++ projects/ImageView/src/imageview/FlagController.java
@@ -0,0 +1,76 @@
+package imageview;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class FlagController implements Initializable {
+
+    String[] Imagenames = new String[6];
+    String[] Imagepaths = new String[6];
+    int currentImageIndex=0;
+    @FXML
+    ImageView flag;
+    @FXML
+    Label results;
+    @FXML
+    ComboBox images;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        images.getItems().add("Az");
+        images.getItems().add("Rus");
+        images.getItems().add("Turkiye");
+        images.getItems().add("Amerika");
+        images.getItems().add("Ingiltere");
+        images.getItems().add("Almaniya");
+        
+    
+        Imagenames[0] = "Az";
+        Imagenames[1] = "Rus";
+        Imagenames[2] = "Turkiye";
+        Imagenames[3] = "Amerika";
+        Imagenames[4] = "Ingiltere";
+        Imagenames[5] = "Almaniya";
+       
+        
+        Imagepaths[0] = "C:\\Users\\Cavid\\Documents\\Github\\project-files-cavid-7675\\projects\\ImageView\\images\\Azerbaycan.jpeg";
+        Imagepaths[1] = "C:\\Users\\Cavid\\Documents\\Github\\project-files-cavid-7675\\projects\\ImageView\\images\\rusiya.png";
+        Imagepaths[2] = "C:\\Users\\Cavid\\Documents\\Github\\project-files-cavid-7675\\projects\\ImageView\\images\\turkiye.png";
+        Imagepaths[3] = "C:\\Users\\Cavid\\Documents\\Github\\project-files-cavid-7675\\projects\\ImageView\\images\\Amerika.png";
+        Imagepaths[4] = "C:\\Users\\Cavid\\Documents\\Github\\project-files-cavid-7675\\projects\\ImageView\\images\\Ingiltere.png";
+        Imagepaths[5] = "C:\\Users\\Cavid\\Documents\\Github\\project-files-cavid-7675\\projects\\ImageView\\images\\Almaniya.png";
+       
+    }
+
+    @FXML
+    private void show(ActionEvent event) throws FileNotFoundException {
+        for (int i = 0; i <= Imagenames.length; i++) {
+            
+        
+        String selected = images.getSelectionModel().getSelectedItem().toString();
+        if(selected==Imagenames[currentImageIndex]){
+         showimage();   
+            
+        }else{
+           currentImageIndex++; 
+        }
+        }
+        currentImageIndex=0;
+    }
+
+    private void showimage() throws FileNotFoundException {
+       FileInputStream input2 = new FileInputStream(Imagepaths[currentImageIndex]);
+          Image image2 = new Image(input2);
+flag.setImage(image2); 
+results.setText(Imagenames[currentImageIndex]);
+    }
+}
