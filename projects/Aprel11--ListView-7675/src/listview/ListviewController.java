f9afc5a2a37dd3ae0ae332986a906706db34549b
cavid-huseynzade-7675 <alicavid826@gmai;.com>
2020-07-14 19:29:20 +0400

    Update ListviewController.java

--- projects/Aprel11--ListView-7675/src/listview/ListviewController.java
+++ projects/Aprel11--ListView-7675/src/listview/ListviewController.java
@@ -13,7 +13,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
 
 public class ListviewController implements Initializable {
 long cem1;
