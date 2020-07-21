ff6bcd25064219f54429d5843439eee3895ebf03
cavid-huseynzade-7675 <alicavid826@gmai;.com>
2020-07-21 09:17:34 +0400

    Update MainClass.java

--- projects/StudentRegisterCodes/src/com/main/MainClass.java
+++ projects/StudentRegisterCodes/src/com/main/MainClass.java
@@ -9,6 +9,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -32,13 +33,16 @@ public class MainClass extends Application {
         hBox.getChildren().add(label);
         hBox.getChildren().add(ad);
 
-        RadioButton goy = new RadioButton("Goy reng");
-        RadioButton qirmizi = new RadioButton("Qirmizi reng");
-        RadioButton yasil = new RadioButton("Yasil reng");
+        RadioButton goy = new RadioButton("Goy");
+        RadioButton qirmizi = new RadioButton("Qirmizi");
+        RadioButton yasil = new RadioButton("Yasil");
+        RadioButton ag = new RadioButton("Ag");
         ToggleGroup qrup1 = new ToggleGroup();
         goy.setToggleGroup(qrup1);
         qirmizi.setToggleGroup(qrup1);
         yasil.setToggleGroup(qrup1);
+        ag.setToggleGroup(qrup1);
+        ToggleButton visible = new ToggleButton("Gizlet");
 
         Label label1 = new Label("      Tevellud          ");
         DatePicker yas = new DatePicker();
@@ -52,7 +56,10 @@ public class MainClass extends Application {
         hBox7.getChildren().add(goy);
         hBox7.getChildren().add(qirmizi);
         hBox7.getChildren().add(yasil);
-
+        hBox7.getChildren().add(ag);
+        hBox7.getChildren().add(visible);
+        
+        
         Label label2 = new Label("                             Ad           ");
         Label label3 = new Label("                            Tevellud          ");
         HBox hBox3 = new HBox();
@@ -84,32 +91,59 @@ public class MainClass extends Application {
             }
 
         });
+   visible.setOnAction((e) -> {
+      boolean secili=visible.isSelected();
+      if(!secili){
+hBox.setVisible(secili);
+hBox1.setVisible(secili);
+hBox3.setVisible(secili);
+hBox4.setVisible(secili);
+myButton.setVisible(secili);
+goy.setVisible(secili);
+qirmizi.setVisible(secili);
+yasil.setVisible(secili);
+ag.setVisible(secili);
+visible.setText("Goster");
 
+      }else{
+ hBox.setVisible(secili);
+hBox1.setVisible(secili);
+hBox3.setVisible(secili);
+hBox4.setVisible(secili);
+myButton.setVisible(secili);
+goy.setVisible(secili);
+qirmizi.setVisible(secili);
+yasil.setVisible(secili);
+ag.setVisible(secili);
+  visible.setText("Gizlet");
+      }
+          
+          
+      
+          
+          
+        });
         VBox vBox = new VBox();
         vBox.getChildren().add(hBox);
         vBox.getChildren().add(hBox1);
         vBox.getChildren().add(hBox7);
         vBox.getChildren().add(hBox3);
         vBox.getChildren().add(hBox4);
-        Scene scene = new Scene(vBox, 700,700);
+        Scene scene = new Scene(vBox, 700, 700);
 
         goy.setOnAction((e) -> {
-   vBox.setStyle("-fx-background-color:blue");
+            vBox.setStyle("-fx-background-color:blue");
         });
-     
 
+        qirmizi.setOnAction((e) -> {
+            vBox.setStyle("-fx-background-color:red");
+        });
 
-    
-
-       
-           qirmizi.setOnAction((e) -> {
-        vBox.setStyle("-fx-background-color:red");
-  });
-           
-           
-       
-  yasil.setOnAction((e) -> {
- vBox.setStyle("-fx-background-color:green");
+        yasil.setOnAction((e) -> {
+            vBox.setStyle("-fx-background-color:green");
+        });
+        ag.setOnAction((e) -> {
+            vBox.setStyle("-fx-background-color:white");
         });
 
         ps.setScene(scene);
