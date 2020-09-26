ee25a48299216af5156610ca2b5f56bc060f2c52
cavid-huseynzade-7675 <alicavid826@gmai;.com>
2020-09-26 12:11:36 +0400

    Studentregister  created

---
+++ FXMl/Studentsregister-aprel11-cavid-7675/src/az/developia/student/MainClass.java
@@ -0,0 +1,28 @@
+
+package az.developia.student;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class MainClass extends Application{
+    public static void main(String[] args) {
+        launch(args);
+    }
+    @Override
+    public void start(Stage s) throws Exception {
+      s.setTitle("Studentregister");
+        FXMLLoader loader=new FXMLLoader(getClass().getResource("view/login.fxml"));
+        Parent root=loader.load();
+        Scene scene= new Scene(root);
+        s.setScene(scene);
+        s.show();  
+    
+    
+    
+    }
+    
+}
