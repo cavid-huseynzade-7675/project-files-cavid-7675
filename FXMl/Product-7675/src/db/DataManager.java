18388e6ae91b2cc4593ea3980d63b20803c52a29
cavid-huseynzade-7675 <alicavid826@gmai;.com>
2020-10-22 14:21:34 +0400

    product created

---
+++ FXMl/Product-7675/src/db/DataManager.java
@@ -0,0 +1,39 @@
+
+package db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class DataManager {
+    //burda mysqle connect eleyirik ki her defe tezden connect etmeyek lazim
+    //olanda metod cagirsinlar
+  private static final DataManager dataManager =new DataManager();
+  private Connection connection;
+
+    public static DataManager getDataManager() {
+        return dataManager;
+    }
+  
+  private DataManager(){
+connect();
+}
+
+  
+  
+  private  void connect(){
+      try {
+        this.connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/product", "root", "1234");
+          System.out.println("succes connection"); 
+          
+      
+      } catch (Exception e) {
+        
+          
+      }
+  }
+  
+   public Connection getConnection() {
+        return this.connection;
+    }
+}
