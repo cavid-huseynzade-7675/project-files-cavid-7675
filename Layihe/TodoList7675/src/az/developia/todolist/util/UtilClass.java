fbfa1feb0d094f55411d92773d20a393cb4834ff
cavid-huseynzade-7675 <alicavid826@gmai;.com>
2020-11-16 15:15:31 +0400

    UtilClass confirm Dialog

--- Layihe/TodoList7675/src/az/developia/todolist/util/UtilClass.java
+++ Layihe/TodoList7675/src/az/developia/todolist/util/UtilClass.java
@@ -5,10 +5,29 @@
  */
 package az.developia.todolist.util;
 
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Cavid
  */
 public class UtilClass {
-    
+     public static boolean confirmDialog(String message) {
+        boolean result = false;
+         JDialog.setDefaultLookAndFeelDecorated(true);
+        int response = JOptionPane.showConfirmDialog(null, message, "", JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE);
+        if (response == JOptionPane.NO_OPTION) {
+            result = false;
+
+        } else if (response == JOptionPane.YES_OPTION) {
+            result = true;
+
+        } else if (response == JOptionPane.CLOSED_OPTION) {
+
+            result = false;
+        }
+        return result;
+    } 
 }
