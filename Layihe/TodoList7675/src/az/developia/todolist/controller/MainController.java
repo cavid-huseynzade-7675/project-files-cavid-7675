77186ef9c24352371eccc334c03771d98cd8dc40
cavid-huseynzade-7675 <alicavid826@gmai;.com>
2020-11-17 08:31:07 +0400

    create main fxml and controller

---
+++ Layihe/TodoList7675/src/az/developia/todolist/controller/MainController.java
@@ -0,0 +1,17 @@
+
+package az.developia.todolist.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class MainController implements Initializable {
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+}
