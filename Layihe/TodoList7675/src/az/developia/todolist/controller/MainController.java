a508e7fd18c29d9bb680ad70ebdf47e6588e50dd
cavid-huseynzade-7675 <alicavid826@gmail.com>
2020-11-28 18:03:56 +0400

    Revert "Update MainController.java"
    This reverts commit de517f24f9a1895d1183ba30b93781bbf30f1f75.

--- Layihe/TodoList7675/src/az/developia/todolist/controller/MainController.java
+++ Layihe/TodoList7675/src/az/developia/todolist/controller/MainController.java
@@ -247,7 +247,7 @@ if(UtilClass.confirmDialog("Eminsiniz")){
     }
      
         if (save) {
-          uzunluq=ChronoUnit.DAYS.between(tarixDatePicker.getValue(),gunDatePicker1.getValue() );
+          long uzunluq=ChronoUnit.DAYS.between(tarixDatePicker.getValue(),gunDatePicker1.getValue() );
          
         }
                if (uzunluq<0) {
