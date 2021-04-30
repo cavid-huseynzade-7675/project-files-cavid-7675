2742b55153d42cdb590c918118531403b98dbec0
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-04-30 18:58:39 +0400

    uptade ders-123

--- html-css/ders-123/script.js
+++ html-css/ders-123/script.js
@@ -1,21 +1,52 @@
-var modal = document.getElementById("myModal");
+var modal = document.getElementById('myModal');
 
 
-var btn = document.getElementById("myBtn");
+var btn = document.getElementById('myBtn');
 
-var span = document.getElementsByClassName("close")[0];
+var span = document.getElementsByClassName('close')[0];
 
 
-btn.onclick = function() {
-  modal.style.display = "block";
+btn.onclick = function () {
+  modal.style.display = 'block';
 }
 
-span.onclick = function() {
-  modal.style.display = "none";
+span.onclick = function () {
+  modal.style.display = 'none';
 }
 
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
-    modal.style.display = "none";
+    modal.style.display = 'none';
   }
 }
+
+
+
+function evvel() {
+  $('#mb').css("display", "block");
+  $('#mb2').css("display", "none");
+  $('#1pageli').addClass('active');
+  $('#2pageli').removeClass('active');
+};
+
+function page1() {
+  $('#mb').css("display", "block");
+  $('#mb2').css("display", "none");
+  $('#1pageli').addClass('active');
+  $('#2pageli').removeClass('active');
+};
+
+function page2() {
+  console.log($('#mb').height());
+  $('#mb2').css("display", "block");
+  $('#mb').css("display", "none");
+  $('#2pageli').addClass('active');
+  $('#1pageli').removeClass('active');
+};
+
+function sonra() {
+  $('#mb2').css("display", "block");
+  $('#mb').css("display", "none");
+  $('#2pageli').addClass('active');
+  $('#1pageli').removeClass('active');
+};
