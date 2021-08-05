e3209bcce95739b47fd454f89efcae590111c3bc
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-08-05 15:49:57 +0400

    shop-save

--- spring/angular/shopProject/src/app/shop-save/shop-save.component.ts
+++ spring/angular/shopProject/src/app/shop-save/shop-save.component.ts
@@ -1,4 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { API_URL } from '../constant';
+import { Shop } from '../models/shop';
+import { ShopService } from '../shop.service';
 
 @Component({
   selector: 'app-shop-save',
@@ -7,9 +12,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopSaveComponent implements OnInit {
 
-  constructor() { }
+  shop: Shop = new Shop();
+  minimum:number=3;
+maksimum:number=30;
+  constructor(
+    private service: ShopService,
+    private http:HttpClient,
+    private router:Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onSaveShop() {
+   // this.service.tasks.push(this.task);
+   this.http.post<Shop>(API_URL+'/shops',this.shop).subscribe(
+resp=>{
+ // this.service.TaskAdded.emit(resp);
+ this.router.navigate(['shops']);
+}
+
+   );
   }
 
 }
