d43f482bdc0ecb5174516c4bc7be333217c76733
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-08-10 09:59:13 +0400

    add app routing sign-in

--- spring/angular/shopProject/src/app/app-routing.module.ts
+++ spring/angular/shopProject/src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { ShopListComponent } from './shop-list/shop-list.component';
 import { ShopSaveComponent } from './shop-save/shop-save.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [
 
   { path: 'shop-save', component:ShopSaveComponent },
   { path: 'shops', component: ShopListComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'logout', component: LogoutComponent }
+  { path: 'logout', component: LogoutComponent },
+  { path: 'sign-in', component: SignInComponent }
 ];
 
 @NgModule({
