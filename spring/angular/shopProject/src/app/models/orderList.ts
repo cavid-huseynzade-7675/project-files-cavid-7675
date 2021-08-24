c97848eba8fec8bb16ccaf9bc94ca11733d3b58a
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-08-24 17:48:29 +0400

    Create orderList.ts

---
+++ spring/angular/shopProject/src/app/models/orderList.ts
@@ -0,0 +1,14 @@
+export class OrderList {
+    public id: number = 0;
+    public adress: string = '';
+    public email: string = '';
+    public name: string = '';
+    public note: string = '';
+    public phone: string = '';
+    public date: Date = new Date();
+    public allPrice:number =0;
+    public price :number=0;
+    public count :number=0;
+    public allPriceInaAllpriceColumn :number=0;
+  }
+  
