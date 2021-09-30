f121c68c242c2d2a57690929055dc3a75378ca3c
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-09-30 17:46:40 +0400

    Addim 10

--- Layihe-Test/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/BookRestController.java
+++ Layihe-Test/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/BookRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +22,10 @@ private BookDao bookDao;
     @GetMapping
     public List<Book> findAll() {
         return bookDao.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Book findById(@PathVariable(name="id") Integer id) {
+        return bookDao.findById(id).get();
     }
 }
