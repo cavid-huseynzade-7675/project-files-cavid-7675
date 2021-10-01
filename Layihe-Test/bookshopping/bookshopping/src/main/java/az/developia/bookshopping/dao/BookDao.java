95c6bd4364897872ec230c37dc890dea3afc28a2
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-10-01 17:27:18 +0400

    Revert "addim 9"
    This reverts commit 0c005973769907196fd1f7637608817c602acada.

--- Layihe-Test/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/dao/BookDao.java
+++
@@ -1,9 +0,0 @@
-package az.developia.bookshopping.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import az.developia.bookshopping.model.Book;
-
-public interface BookDao extends JpaRepository<Book,Integer> {
-    
-}
