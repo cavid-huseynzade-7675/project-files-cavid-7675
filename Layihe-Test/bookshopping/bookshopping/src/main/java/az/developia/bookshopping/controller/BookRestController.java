346694aa4d629fdc323b3801a0af3a104917f78d
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-10-02 16:35:34 +0400

    yeni

---
+++ Layihe-Test/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/controller/BookRestController.java
@@ -0,0 +1,27 @@
+package az.developia.bookshopping.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import az.developia.bookshopping.dao.BookDao;
+import az.developia.bookshopping.model.Book;
+
+
+@RestController
+@RequestMapping(path = "/books")
+public class BookRestController {
+    
+@Autowired
+private BookDao bookDao;
+
+
+    @GetMapping
+    public List<Book> findAll() {
+        return bookDao.findAll();
+    }
+}
---
+++ Layihe-Test/bookshopping/bookshopping/src/main/java/az/developia/bookshopping/dao/BookDao.java
@@ -0,0 +1,9 @@
+package az.developia.bookshopping.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import az.developia.bookshopping.model.Book;
+
+public interface BookDao extends JpaRepository<Book,Integer>  {
+	
+}
