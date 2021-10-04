d48d0b482f1b8ffd56bcc8b0564f980f281ae0ef
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-10-04 16:27:38 +0400

    addim 19

--- Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/controller/HomeController.java
+++ Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     
 
-    @GetMapping(path = "/home")
+    @GetMapping(path = {"/home","/"})
     public String showHomePage() {
         return "home";
     }
