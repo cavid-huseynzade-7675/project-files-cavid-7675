0980e2dcd38942c5e1ccc94e0c2ee94e83c5ec13
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-10-09 09:31:03 +0400

    addim 44

--- Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/controller/CusromerController.java
+++ Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/controller/CusromerController.java
@@ -1,6 +1,9 @@
 package az.developia.bookshoopinfgor.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -8,8 +11,12 @@ public class CusromerController {
     
      
     @GetMapping(path = "/customer")
-    public String showCustomersPage(){
-
+    public String showCustomersPage( Model model){
+ArrayList<String> books=new ArrayList<>();
+for (int i = 0; i < 100; i++) {
+    books.add(" ");
+}
+model.addAttribute("books", books);
         return "customer";
     }
 }
