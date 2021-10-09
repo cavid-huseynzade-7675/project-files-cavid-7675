51ba433ea457050374c80e98882a419ef2faef86
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-10-09 09:57:02 +0400

    addim 50

--- Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/controller/CusromerController.java
+++ Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/controller/CusromerController.java
@@ -1,21 +1,27 @@
 package az.developia.bookshoopinfgor.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import az.developia.bookshoopinfgor.dao.BookDao;
+import az.developia.bookshoopinfgor.model.BookModel;
+
 @Controller
 public class CusromerController {
     
+    @Autowired
+    private BookDao bookDao;
      
     @GetMapping(path = "/customer")
     public String showCustomersPage( Model model){
-ArrayList<String> books=new ArrayList<>();
-for (int i = 0; i < 100; i++) {
-    books.add(" ");
-}
+
+
+        List<BookModel> books=bookDao.findAll();
 model.addAttribute("books", books);
         return "customer";
     }
