e3bb60f22aed0ab446f916216f88a72b560296a8
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-10-12 15:59:05 +0400

    addim 59

--- Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/config/SecurityConfig.java
+++ Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/config/SecurityConfig.java
@@ -20,9 +20,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
        
         http.csrf().disable().authorizeRequests()
-        .antMatchers(HttpMethod.GET, "/").permitAll()
+        .antMatchers(HttpMethod.GET, "/").permitAll().
+        antMatchers(HttpMethod.GET, "/create-account").permitAll().
+        antMatchers(HttpMethod.POST, "/create-account-procces").permitAll()
         .anyRequest().authenticated()
-        .and().formLogin().loginPage("/show-login").loginProcessingUrl("/authenticate-user").permitAll()
+        .and().formLogin().loginPage("/show-login").loginProcessingUrl("/authenticate-user").
+        permitAll()
         .and().logout().permitAll();
     }
     
--- Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/controller/UserController.java
+++ Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/controller/UserController.java
@@ -1,7 +1,15 @@
 package az.developia.bookshoopinfgor.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import az.developia.bookshoopinfgor.model.User;
 
 @Controller
 public class UserController {
@@ -11,5 +19,22 @@ public class UserController {
     public String showCustomersPage(){
 
         return "my-custom-login";
+    }
+
+    @GetMapping(path = "create-account")
+    public String showCreateAccountPage(Model model){
+        User  user=new User();
+model.addAttribute("user", user);
+return "create-account";
+    }
+
+    @PostMapping(path = "/create-account-procces")
+    public String saveUser(@Valid @ModelAttribute(name = "user") User user
+    ,BindingResult result,Model model){
+if (result.hasErrors()) {
+    return "create-account";
+};
+System.out.println(user);
+return "redirect:/show-user";
     }
 }
