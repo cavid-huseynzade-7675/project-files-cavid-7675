1c5d6794329f92e11928e202cd48cbb27ffad7a3
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-10-15 08:48:21 +0400

    addim 73

--- Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/dao/UserDao.java
+++ Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/dao/UserDao.java
@@ -7,6 +7,7 @@ import java.sql.ResultSet;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import az.developia.bookshoopinfgor.model.User;
@@ -16,6 +17,8 @@ public class UserDao {
     
     @Autowired
     private DataSource dataSource;
+
+    private BCryptPasswordEncoder passwordEncoder=new BCryptPasswordEncoder();
 
     public boolean createUser(User user){
         boolean userExists=false;
@@ -34,7 +37,7 @@ if (rs.next()) {
     
     ps=conn.prepareStatement("insert into users (username,password,enabled) values(?,?,?);");
     ps.setString(1, user.getUsername());
-    ps.setString(2, "{noop}"+user.getPassword());
+    ps.setString(2, "{bcrypt}"+passwordEncoder.encode(user.getPassword()));
     ps.setInt(3, (byte)1);
     ps.executeUpdate();
     ps.close();
