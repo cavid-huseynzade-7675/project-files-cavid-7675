feec350975e8005b9de57d411419fff972ccd59e
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-10-15 09:08:52 +0400

    addim 76

--- Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/file/StorageService.java
+++ Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/file/StorageService.java
@@ -1,5 +1,21 @@
 package az.developia.bookshoopinfgor.file;
 
-public class StorageService {
+import java.nio.file.Path;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public interface StorageService {
     
+    void init();
+
+    String store(MultipartFile file);
+
+
+    Path load(String filename);
+
+    Resource loadAsResource(String filename);
+
+    void deleteAll();
 }
