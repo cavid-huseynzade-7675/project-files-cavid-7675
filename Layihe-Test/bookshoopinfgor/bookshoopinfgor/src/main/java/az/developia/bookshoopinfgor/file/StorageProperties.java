fc7827348a1428f06486bc237a5e2822b244e2cf
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-10-15 09:02:54 +0400

    addim 75

--- Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/file/StorageProperties.java
+++ Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/file/StorageProperties.java
@@ -1,5 +1,17 @@
 package az.developia.bookshoopinfgor.file;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties("storage")
 public class StorageProperties {
     
+    private String location ="upload-dir-book-shopping-spring-ajax";
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
