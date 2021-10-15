b27ddb9702f11c4898f4d252757be121eef43301
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-10-15 09:29:18 +0400

    addim 77

--- Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/file/FileSystemStorageService.java
+++ Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/file/FileSystemStorageService.java
@@ -1,5 +1,41 @@
 package az.developia.bookshoopinfgor.file;
 
-public class FileSystemStorageService {
+import java.nio.file.Path;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileSystemStorageService implements StorageService {
+
+    @Override
+    public void deleteAll() {
+  
+    }
+
+    @Override
+    public void init() {
+                
+    }
+
+    @Override
+    public Path load(String filename) {
+        
+        return null;
+    }
+
+    @Override
+    public Resource loadAsResource(String filename) {
+        
+        return null;
+    }
+
+    @Override
+    public String store(MultipartFile file) {
+        
+        return null;
+    }
     
 }
