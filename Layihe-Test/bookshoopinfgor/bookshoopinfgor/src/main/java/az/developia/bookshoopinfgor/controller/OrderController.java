e0f1fa5214c3049d7b4f33fca10f79e78d2bdb04
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-10-22 19:40:39 +0400

    089. Sifarişlərin siyahısı 1-ci addım

--- Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/controller/OrderController.java
+++ Layihe-Test/bookshoopinfgor/bookshoopinfgor/src/main/java/az/developia/bookshoopinfgor/controller/OrderController.java
@@ -1,19 +1,31 @@
 package az.developia.bookshoopinfgor.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import az.developia.bookshoopinfgor.dao.OrderDAO;
+import az.developia.bookshoopinfgor.model.Order;
 
 @Controller
 public class OrderController {
     
-    @GetMapping(path = "/orders")
-    public String showOrdersPage(){
+    @Autowired
+    private OrderDAO orderDAO;
 
+
+    @GetMapping(path = "/orders")
+    public String showOrdersPage( Model model){
+        List<Order> orders=orderDAO.findAll();
+        model.addAttribute("orders", orders);
         return "orders";
     }
     @GetMapping(path = "/confirm-order")
         public String showConfirmOrderPage(){
-
+         
             return "confirm-order";
         }
     
