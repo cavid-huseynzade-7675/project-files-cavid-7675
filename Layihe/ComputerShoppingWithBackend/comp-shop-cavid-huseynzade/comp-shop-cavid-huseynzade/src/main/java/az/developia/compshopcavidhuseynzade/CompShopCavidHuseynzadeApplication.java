d2b14be1ef03fabf914d95ff78afcd5c558d5d38
cavid-huseynzade-7675 <alicavid826@gmail.com>
2021-11-18 20:41:07 +0400

    Project Created

---
+++ Layihe/ComputerShoppingWithBackend/comp-shop-cavid-huseynzade/comp-shop-cavid-huseynzade/src/main/java/az/developia/compshopcavidhuseynzade/CompShopCavidHuseynzadeApplication.java
@@ -0,0 +1,13 @@
+package az.developia.compshopcavidhuseynzade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompShopCavidHuseynzadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompShopCavidHuseynzadeApplication.class, args);
+	}
+
+}
